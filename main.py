fbfb584f84f6daa61b9a08f64f181c41904c3e47
bonecracker <benny.tieu@gmail.com>
2016-12-04 16:18:15 +0800

    Can now identify Rich Text

--- main.py
+++ main.py
@@ -1,36 +1,57 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from subprocess import Popen, PIPE
-import gi, time
+from subprocess import run, PIPE
+import gi, time, sys
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk
 
 # Remove previous notification
-Popen(['pkill', "notify-osd"])
+run(['pkill', "notify-osd"])
 
 cb = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
+hasTargets, targets = cb.wait_for_targets()
 
-try: # Clipboard is a string 
-	cbText = cb.wait_for_text()
-	# Display newline and tabs correctly in notify-send
-	cbText.replace("\n", "\\n")
-	cbText.replace("\t", "\\t")
-	
-	# Display first 300 characters only
-	if len(cbText) > 300: 
-		cbText = cbText[:300-len(cbText)]
-		cbText = cbText + "..."
-	
-	Popen(['notify-send', "Clipboard Content", cbText])
+# Display first 300 characters only
+def shortText(text):
+	if len(text) > 300: 
+		text = text[:300-len(text)]
+		text = text + "..."
+	return text
 
-except: # Clipboard is an image
-	imgPath = "/tmp/img.png"
-	cbImage = cb.wait_for_image()
-	cbImage.savev(imgPath, "png",[],[])
-	
-	Popen(['notify-send', '-i', imgPath, "Clipboard Content", "[Image]"])
+if not hasTargets: 
+	run(['notify-send', '-u', 'critical',"Clipboard is empty"])
+	sys.exit()
+else:
+	for target in targets:
+		'''
+		# Uncomment below to check different gtk target types
+		print(target)
+		'''
+		# Clipboard contains Rich Text
+		if str(target) == "text/rtf":
+			cbText = cb.wait_for_text()
+			cbText = shortText(cbText) 
+			run(['notify-send', '-u', 'critical',"Clipboard [Rich Text]", cbText])
+			break
+		# Clipboard contains Plain Text
+		elif str(target) == "text/plain" or str(target) == "TEXT" or str(target) == "STRING" :
+			cbText = cb.wait_for_text()
+			cbText = shortText(cbText)
+			
+			# Display newline and tabs correctly in notify-send
+			cbText.replace("\n", "\\n")
+			cbText.replace("\t", "\\t")
+			
+			run(['notify-send', '-u', 'critical',"Clipboard [Plain Text]", cbText])
+			break
 
-	#TODO: Remove tmp image after showing it
+		# Clipboard contains an Image
+		elif "image/" in str(target):
+			#jpegs files are generally smaller than png = faster exec
+			imgType = "jpeg"
+			imgPath = "/tmp/img."+imgType #Save the image in tmp folder
+			cbImage = cb.wait_for_image()
+			cbImage.savev(imgPath, imgType,[],[])
 
-#time.sleep(5)
-#Popen(['pkill', "notify-osd"])
+			run(['notify-send', '-u', 'critical' ,'-i', imgPath, "Clipboard [Image]"])
+			break
